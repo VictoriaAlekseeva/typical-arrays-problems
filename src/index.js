89bb6b90d37d8eeebfbbbc20cb06d075cf7cdd55
VictoriaAlekseeva <77786771+VictoriaAlekseeva@users.noreply.github.com>
2021-03-15 15:04:57 +0100

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
-
 exports.min = function min (array) {
-  return 0;
+
+  if ((array.length !== undefined) || (array.length !== NaN) || (array.length !== null)) // доделат ьпроверку на пустой массив
+  {
+    let minInArray = array[0];
+    for (i = 0; i < array.length; i++) {
+    if (array[i] < minInArray)
+    minInArray = array[i];
+    }
+
+  return minInArray; }
+
+else return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+
+
+  let maxInArray = array[0];
+  for (i = 0; i < array.length; i++) {
+    if (array[i] > maxInArray)
+    maxInArray = array[i];
+  }
+  return maxInArray;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+
+
+  let sum = 0;
+
+  for (i = 0; i < array.length; i++) {
+    sum = sum + (+array[i].toFixed());
+  }
+  let avgOfArray = sum / array.length;
+
+  return avgOfArray;
 }
+
